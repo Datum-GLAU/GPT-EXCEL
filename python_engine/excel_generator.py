8de7252790c2712dbbf7e3675f309edf894b5cc9
Kakul Mittal <kakulmittal2006@gmail.com>
2026-03-28 18:49:53 +0530

    Update excel_generator.py

--- python_engine/excel_generator.py
+++ python_engine/excel_generator.py
@@ -1,14 +1,11 @@
 # -------- BASIC EXCEL GENERATION (optional demo) --------
-
 # def create_excel():
 #     data = {
 #         "Name": ["Aman", "Riya", "Rahul", "Sneha"],
 #         "Score": [85, 90, 78, 92]
 #     }
-
 #     df = pd.DataFrame(data)
 #     df.to_excel("output.xlsx", index=False)
-
 #     return "Excel file generated successfully"
 
 import pandas as pd
@@ -19,3 +16,33 @@ def read_excel(file_path):
         return df.to_dict(orient="records")
     except Exception as e:
         return {"error": str(e)}
+
+
+def generate_advanced_excel(file_path):
+
+    import pandas as pd
+
+    df = pd.read_excel(file_path)
+
+    with pd.ExcelWriter("final_output.xlsx", engine="xlsxwriter") as writer:
+
+        df.to_excel(writer, sheet_name="Raw Data", index=False)
+
+        summary = df.describe()
+        summary.to_excel(writer, sheet_name="Summary")
+
+        workbook = writer.book
+        worksheet = writer.sheets["Raw Data"]
+
+        header_format = workbook.add_format({
+            'bold': True,
+            'border': 1
+        })
+
+        for col_num, value in enumerate(df.columns.values):
+            worksheet.write(0, col_num, value, header_format)
+
+        for i, col in enumerate(df.columns):
+            worksheet.set_column(i, i, 20)
+
+    return "Advanced Excel generated"
